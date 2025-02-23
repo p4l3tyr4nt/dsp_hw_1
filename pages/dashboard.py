25875ddac0fe7006e8f00caf8c49e9bd59f293e0
Egor Zubachev <p4l3tyr4nt@gmail.com>
2025-02-23 21:10:21 +0300

    Add callback for num buttons

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,8 +1,8 @@
 import dash
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc, callback, ctx, Output, Input, State, ALL, no_update
 import dash_bootstrap_components as dbc
 
-dash.register_page(__name__, path="/")
+dash.register_page(__name__, path='/')
 
 def layout():
     return index()
@@ -10,23 +10,24 @@ def layout():
 def index():
 
     mode_choice_btn = html.Div(children=[
-        dbc.Label("Sample rate kHz", html_for="sample-rate-slider"),
-        dcc.Slider(id="sample-rate-input", min=2, max=10, step=1, value=10),
-        html.H4("Mode choice"),
+        dbc.Label('Sample rate kHz', html_for='sample-rate-slider'),
+        dcc.Slider(id='sample-rate-input', min=2, max=10, step=1, value=10),
+        html.H4('Mode choice'),
         dcc.RadioItems(
-            options=["Single", "Multi"],
-            value="Single",
-            id="mode-choice",
-            className="mode-choice"
+            options=['Single', 'Multi'],
+            value='Single',
+            id='mode-choice',
+            className='mode-choice'
         ),
     ])
 
     number_info = html.Div(children=
         [
-            dbc.Label("Number: -"),
+            dbc.Label('Number: '),
+            html.Div('', id='number'),
             html.Div(children=
                 [
-                    dbc.Button("Call", id='btn-call', color='success', n_clicks=0, className='input-btn'),
+                    dbc.Button('Call', id='btn-call', color='success', n_clicks=0, className='input-btn'),
                 ]),
         ],
         className='number-info hidden',
@@ -36,13 +37,13 @@ def index():
         [
             dbc.Button(
                 symbol,
-                id='btn' + symbol,
-                color="success",
+                id={'type': 'btn', 'index': symbol},
+                color='success',
                 n_clicks=0,
                 className='input-btn'
             ) for symbol in '123456789*0#'
         ],
-        className="input-menu",
+        className='input-menu',
     )
 
     side_menu_form = html.Div(
@@ -51,7 +52,7 @@ def index():
             number_info,
             input_menu,
         ],
-        className="sidenav text-white"
+        className='sidenav text-white'
     )
 
     graph = dcc.Graph(id='graph-content', config={'displayModeBar': False})
@@ -62,3 +63,17 @@ def index():
             graph,
         ],
         className=__name__)
+
+@callback(
+    Output('number', 'children'),
+    Output('graph-content', 'figure'),
+    Input({'type': 'btn', 'index': ALL}, 'n_clicks'),
+    State('mode-choice', 'value'),
+    State('sample-rate-input', 'value'),
+    prevent_initial_call=True,
+)
+def num_click(_, mode, sample_rate):
+
+    btn_index = eval(ctx.triggered[0]['prop_id'].split('.')[0])['index']
+
+    return no_update, no_update
