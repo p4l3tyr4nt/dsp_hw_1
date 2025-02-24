ac18c3d54a7efb43d59eee7b2b0b1fbd8702808b
Egor Zubachev <p4l3tyr4nt@gmail.com>
2025-02-24 03:58:44 +0300

    Add audio file generation when press key in single mode

--- dtmf.py
+++ dtmf.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
+from scipy.io.wavfile import write
 
 freq_array_1 = np.array([1209, 1336, 1477])
 freq_array_2 = np.array([697, 770, 852, 941])
@@ -8,19 +9,24 @@ freq_array_2 = np.array([697, 770, 852, 941])
 freq_matrix = np.linspace(1, 9, num=9, dtype=int).reshape(3, 3)
 
 class Signal:
-    def __init__(self, num: str, x: np.ndarray, t: np.ndarray, part: float):
+    def __init__(self, num: str, x: np.ndarray, t: np.ndarray):
         self.num = num
         self.x = x
         self.t = t
-        self.last = int(len(x) * part)
     
-    def get_graph(self):
-        df = pd.DataFrame({'Signal': self.x[:self.last], 't': self.t[:self.last]})
+    def get_graph(self, part: float = 0.125):
+        last = int(len(self.x) * part)
+        df = pd.DataFrame({'Signal': self.x[:last], 't': self.t[:last]})
         return px.line(df, x='t', y='Signal', title=f'Signal for {self.num}')
-
-def get_signal(num: str, sample_rate_kHz: int, duration: float, part: float) -> Signal:
     
-    t = np.linspace(0, duration, num=round(duration * sample_rate_kHz * 1e3))
+    def generate_audio(self, sample_rate_kHz: int):
+        amplitude = np.iinfo(np.int16).max
+        audio_data = amplitude * self.x / np.max(self.x)
+        write('audio/dtmf.wav', int(sample_rate_kHz * 1e3), audio_data.astype(np.int16))
+
+def get_signal(num: str, sample_rate_kHz: int, duration: float = 1) -> Signal:
+
+    t = np.linspace(0, duration, num=int(duration * sample_rate_kHz * 1e3))
 
     if num.isdigit():
         if num == '0':
@@ -45,4 +51,4 @@ def get_signal(num: str, sample_rate_kHz: int, duration: float, part: float) -> 
 
     x = np.sin(2 * np.pi * freq_1 * t) + np.sin(2 * np.pi * freq_2 * t)
 
-    return Signal(num, x, t, part)
+    return Signal(num, x, t)
--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,4 +1,6 @@
 import dash
+import dash_player
+import base64
 from dash import Dash, html, dcc, callback, ctx, Output, Input, State, ALL, no_update
 import dash_bootstrap_components as dbc
 
@@ -12,11 +14,11 @@ def layout():
 def index():
 
     mode_choice_btn = html.Div(children=[
-        dbc.Label('Sample rate kHz', html_for='sample-rate-slider'),
-        dcc.Slider(id='sample-rate-input', min=2, max=10, step=1, value=10),
+        html.H4('Sample rate kHz'),
+        dcc.Slider(id='sample-rate-input', min=2, max=20, step=2, value=10),
         html.H4('Mode choice'),
         dcc.RadioItems(
-            options=['Single', 'Multi'],
+            options=['Single', 'Multiple'],
             value='Single',
             id='mode-choice',
             className='mode-choice'
@@ -25,15 +27,17 @@ def index():
 
     number_info = html.Div(id='number-info', children=
         [
-            dbc.Label('Number: '),
-            html.Div('', id='number'),
+            html.H4('Number: '),
+            html.H5('', id='number'),
             html.Div(children=
                 [
                     dbc.Button('Call', id='btn-call', color='success', n_clicks=0, className='input-btn'),
                 ]),
         ],
-        className='number-info hidden',
-        )
+        className='hidden',
+    )
+    
+    player = html.Audio(id='audio-player', src='', controls=True, autoPlay=True, className='player')
 
     input_menu =  html.Div(
         [
@@ -52,6 +56,7 @@ def index():
         [
             mode_choice_btn,
             number_info,
+            player,
             input_menu,
         ],
         className='sidenav text-white'
@@ -67,41 +72,66 @@ def index():
         className=__name__)
 
 @callback(
-    Output('number', 'children'),
-    Output('graph-content', 'figure'),
+    [
+        Output('number', 'children', allow_duplicate=True),
+        Output('graph-content', 'figure'),
+        Output('audio-player', 'src', allow_duplicate=True),
+    ],
     Input({'type': 'btn', 'index': ALL}, 'n_clicks'),
-    State('mode-choice', 'value'),
-    State('sample-rate-input', 'value'),
+    [
+        State('mode-choice', 'value'),
+        State('sample-rate-input', 'value'),
+        State('number', 'children'),
+    ],
     prevent_initial_call=True,
 )
-def num_click(_, mode, sample_rate_kHz):
+def num_click(_, mode, sample_rate_kHz, number):
 
     btn_index = eval(ctx.triggered[0]['prop_id'].split('.')[0])['index']
 
     if mode == 'Single':
 
-        signal = get_signal(btn_index, sample_rate_kHz, 0.5, 0.125)
+        signal = get_signal(btn_index, sample_rate_kHz)
 
-        return '', signal.get_graph()
+        signal.generate_audio(sample_rate_kHz)
 
-    return no_update, no_update
+        encoded_sound = base64.b64encode(open('audio/dtmf.wav', 'rb').read())
+        src = 'data:audio/wav;base64,{}'.format(encoded_sound.decode())
+
+        return '', signal.get_graph(), src
+    
+    elif mode == 'Multiple':
+
+        return number + btn_index, no_update, no_update
+
+    return no_update, no_update, no_update
 
 @callback(
+    Output('audio-player', 'src', allow_duplicate=True),
     Input('btn-call', 'n_clicks'),
     State('sample-rate-input', 'value'),
     prevent_initial_call=True,
 )
-def call_click(_, sample_rate):
+def call_click(_, sample_rate_kHz):
+
     pass
 
 @callback(
-    Output('number-info', 'className'),
+    [
+        Output('number-info', 'className'),
+        Output('number', 'children', allow_duplicate=True),
+    ],
     Input('mode-choice', 'value'),
     prevent_initial_call=True,
 )
 def mode_choice(mode):
+
     if mode == 'Single':
-        return 'number-info hidden'
-    elif mode == 'Multi':
-        return 'number-info'
-    return no_update
+
+        return 'hidden', ''
+    
+    if mode == 'Multiple':
+
+        return 'visible', no_update
+    
+    return no_update, no_update
